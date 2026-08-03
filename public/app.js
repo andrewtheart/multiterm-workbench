05f7174fad6b4c0bdfac42c9382eb4cda1f5f70a
Andrew Stein <andrew1stein@gmail.com>
2026-08-02 23:07:23 -0500

    chore(release): v0.1.56

--- public/app.js
+++ public/app.js
@@ -157,7 +157,7 @@ const HEADER_ACTIONS = Object.freeze({
 });
 
 // Bumped on each rebuild. See /memories/repo for the convention.
-const APP_VERSION = "0.1.55";
+const APP_VERSION = "0.1.56";
 const TERMINAL_ARTIFACTS_STORAGE_KEY = "multiterm.terminalArtifacts";
 const MIN_FONT_SIZE = 10;
 const MAX_FONT_SIZE = 22;
