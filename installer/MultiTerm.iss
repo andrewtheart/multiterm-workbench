17e76400079b875085fbbf5cd16daf4deaedba26
Andrew Stein <andrew1stein@gmail.com>
2026-08-06 20:00:42 -0500

    chore(release): v0.1.75

--- installer/MultiTerm.iss
+++ installer/MultiTerm.iss
@@ -20,7 +20,7 @@
 ; Output: installer\Output\MultiTerm-Setup-<version>.exe
 
 #define MyAppName "MultiTerm Workbench"
-#define MyAppVersion "0.1.74"
+#define MyAppVersion "0.1.75"
 #define MyAppPublisher "MultiTerm Workbench"
 #define MyAppURL "https://github.com/andrewtheart/multiterm-workbench"
 #define MyScriptFile "Start-MultiTerm.ps1"
--- public/app.js
+++ public/app.js
@@ -227,7 +227,7 @@ const HEADER_ACTIONS = Object.freeze({
 });
 
 // Bumped on each rebuild. See /memories/repo for the convention.
-const APP_VERSION = "0.1.74";
+const APP_VERSION = "0.1.75";
 const AUTOMATIONS_STORAGE_KEY = "multiterm.automations";
 const TERMINAL_ARTIFACTS_STORAGE_KEY = "multiterm.terminalArtifacts";
 const TERMINAL_ANALYTICS_STORAGE_KEY = "multiterm.analytics";
