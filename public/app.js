c6aa94b687c967ae0902e9ba4913d6011faa0a53
Andrew Stein <andrew1stein@gmail.com>
2026-09-03 13:53:19 -0500

    chore(release): v0.1.108

--- public/app.js
+++ public/app.js
@@ -415,7 +415,7 @@ const HEADER_ACTION_SHORTCUT_DEFAULTS = Object.freeze({
 });
 
 // Bumped on each rebuild. See /memories/repo for the convention.
-const APP_VERSION = "0.1.107";
+const APP_VERSION = "0.1.108";
 const AUTOMATIONS_STORAGE_KEY = "multiterm.automations";
 const TERMINAL_ARTIFACTS_STORAGE_KEY = "multiterm.terminalArtifacts";
 const TITLE_SUGGESTION_HISTORY_STORAGE_KEY = "multiterm.titleSuggestionHistory";
